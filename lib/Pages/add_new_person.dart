ca2a37c509d52d2366e2d95a39c13176dd576bbb
anivkuttan <anivkutta196@gmail.com>
2022-09-19 23:26:40 +0530

    will pop scope added

--- lib/Pages/add_new_person.dart
+++ lib/Pages/add_new_person.dart
@@ -38,98 +38,100 @@ class _AddNewPersonState extends State<AddNewPerson> {
         key: formKey,
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              GetBuilder<ImageController>(builder: (controller) {
-                return GestureDetector(
-                  onTap: () {
-                    showModalBottomSheet(
-                        context: context,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(23),
-                        ),
-                        builder: (context) {
-                          return SizedBox(
-                            height: 100,
-                            child: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  IconButton(
-                                    icon: const Icon(Icons.camera, size: 50),
-                                    onPressed: () {
-                                      imageController.pickImage(ImageSource.camera);
-                                      Navigator.pop(context);
-                                    },
-                                  ),
-                                  IconButton(
-                                    icon: const Icon(Icons.library_books_outlined, size: 50),
-                                    onPressed: () {
-                                      imageController.pickImage(ImageSource.gallery);
-                                      Navigator.pop(context);
-                                    },
-                                  ),
-                                ]),
-                          );
-                        });
-                  },
-                  child: ClipOval(
-                    child: SizedBox.fromSize(
-                      size: const Size.fromRadius(80),
-                      child: controller.image == null
-                          ? const CircleAvatar()
-                          : Image.file(
-                              controller.image!,
-                              fit: BoxFit.cover,
-                            ),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                GetBuilder<ImageController>(builder: (controller) {
+                  return GestureDetector(
+                    onTap: () {
+                      showModalBottomSheet(
+                          context: context,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(23),
+                          ),
+                          builder: (context) {
+                            return SizedBox(
+                              height: 100,
+                              child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    IconButton(
+                                      icon: const Icon(Icons.camera, size: 50),
+                                      onPressed: () {
+                                        imageController.pickImage(ImageSource.camera);
+                                        Navigator.pop(context);
+                                      },
+                                    ),
+                                    IconButton(
+                                      icon: const Icon(Icons.library_books_outlined, size: 50),
+                                      onPressed: () {
+                                        imageController.pickImage(ImageSource.gallery);
+                                        Navigator.pop(context);
+                                      },
+                                    ),
+                                  ]),
+                            );
+                          });
+                    },
+                    child: ClipOval(
+                      child: SizedBox.fromSize(
+                        size: const Size.fromRadius(80),
+                        child: controller.image == null
+                            ? const CircleAvatar()
+                            : Image.file(
+                                controller.image!,
+                                fit: BoxFit.cover,
+                              ),
+                      ),
+                    ),
+                  );
+                }),
+                const SizedBox(height: 30),
+                TextFormField(
+                  controller: _nameController,
+                  decoration: InputDecoration(
+                    hintText: 'Name',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(23),
                     ),
                   ),
-                );
-              }),
-              const SizedBox(height: 30),
-              TextFormField(
-                controller: _nameController,
-                decoration: InputDecoration(
-                  hintText: 'Name',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(23),
-                  ),
+                  validator: (currentText) {
+                    if (currentText == null || currentText.isEmpty) {
+                      return "Please Enter Name";
+                    }
+                    return null;
+                  },
                 ),
-                validator: (currentText) {
-                  if (currentText == null || currentText.isEmpty) {
-                    return "Please Enter Name";
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 40),
-              ElevatedButton(
-                onPressed: () {
-                  bool isValidated = formKey.currentState!.validate();
-                  if (isValidated) {
-                    final editedTime = DateTime.now();
-                    String editedTime1 =
-                        "${editedTime.day}/${editedTime.month}/${editedTime.year} Time : ${editedTime.hour}:${editedTime.minute}";
-
-                   
-                    Person newPerson = Person(
-                       
-                        personName: _nameController.text,
-                        personAmount: 0,
-                        listOfTask: [
-                          Task(taskName: "Just Created", taskAmount: 0, editedTime: editedTime1),
-                        ],
-                        personImage: imageController.image == null ? null : imageController.imageAsByts);
-
-                    // personController.personList.add(newPerson);
-                    personController.createPerson(person: newPerson);
-
-                    Navigator.pop(context);
-                  }
-                },
-                child: const Text('Add Person '),
-              )
-            ],
+                const SizedBox(height: 40),
+                ElevatedButton(
+                  onPressed: () {
+                    bool isValidated = formKey.currentState!.validate();
+                    if (isValidated) {
+                      final editedTime = DateTime.now();
+                      String editedTime1 =
+                          "${editedTime.day}/${editedTime.month}/${editedTime.year} Time : ${editedTime.hour}:${editedTime.minute}";
+          
+                     
+                      Person newPerson = Person(
+                         
+                          personName: _nameController.text,
+                          personAmount: 0,
+                          listOfTask: [
+                            Task(taskName: "Just Created", taskAmount: 0, editedTime: editedTime1),
+                          ],
+                          personImage: imageController.image == null ? null : imageController.imageAsByts);
+          
+                      // personController.personList.add(newPerson);
+                      personController.createPerson(person: newPerson);
+          
+                      Navigator.pop(context);
+                    }
+                  },
+                  child: const Text('Add Person '),
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -18,40 +18,83 @@ class _HomePageState extends State<HomePage> {
     super.dispose();
   }
 
+  Future<bool?> askUserToExit() async {
+    return showDialog<bool?>(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            content: SizedBox(
+                height: 100,
+                width: 400,
+                child: Column(
+                  children: [
+                    const Text('Do You Want to Exit the app?'),
+                    Row(
+                      children: [
+                        const Spacer(),
+                        TextButton(
+                          child: const Text('Yes'),
+                          onPressed: () {
+                            Navigator.pop(context, true);
+                          },
+                        ),
+                        TextButton(
+                          child: const Text(
+                            'No',
+                            style: TextStyle(color: Colors.red),
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context, false);
+                          },
+                        ),
+                      ],
+                    )
+                  ],
+                )),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('HomePage'),
-          centerTitle: true,
-          leading: IconButton(
-            icon: const Icon(Icons.menu),
-            onPressed: () {},
-          ),
-          actions: [
-            IconButton(
+    return WillPopScope(
+      onWillPop: () async {
+        final userChoice = await askUserToExit();
+        return userChoice ?? false;
+      },
+      child: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('HomePage'),
+            centerTitle: true,
+            leading: IconButton(
+              icon: const Icon(Icons.menu),
               onPressed: () {},
-              icon: const Icon(Icons.dark_mode),
-            )
-          ],
-          bottom: const TabBar(tabs: [
-            Tab(
-              child: Text('Home'),
             ),
-            Tab(
-              child: Text('Quick'),
-            ),
-            Tab(child: Text('Cleaned')),
-          ]),
-        ),
-        body: TabBarView(
-          children: [
-            HomePageTabView(),
-            QuickPageTabView(),
-            const ClearPageTabView(),
-          ],
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.dark_mode),
+              )
+            ],
+            bottom: const TabBar(tabs: [
+              Tab(
+                child: Text('Home'),
+              ),
+              Tab(
+                child: Text('Quick'),
+              ),
+              Tab(child: Text('Cleaned')),
+            ]),
+          ),
+          body: TabBarView(
+            children: [
+              HomePageTabView(),
+              QuickPageTabView(),
+              const ClearPageTabView(),
+            ],
+          ),
         ),
       ),
     );
